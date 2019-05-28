30ff288e3111d704e0977c704bfa596b0c2a5e18
imdhruvgupta <gdhruv167@gmail.com>
2019-05-28 13:32:40 +0530

    [CHANGE] user creation controller and route

---
+++ src/controller/user.js
@@ -0,0 +1,27 @@
+const {Users} = require('../models');
+
+async function createUser(userOpts) {
+    if(!userOpts.username) {
+        throw new Error("Did not supply username");
+    }
+    if(!userOpts.email) {
+        throw new Error("Did not supply email");
+    }
+    if(!userOpts.password) {
+        throw new Error("Did not supply password");
+    }
+
+    const user = await Users.create({
+        ...userOpts
+    })
+
+    if(!user) {
+        throw new Error("Error creating user");
+    }
+
+    return user.get();
+}
+
+module.exports = {
+    createUser
+}
--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -1,4 +1,11 @@
-const {Router} = require("express");
+const {Router} = require('express');
 const route = Router();
+const {createUser} = require('../../controller/user');
+
+route.post('/', async (req, res) => {
+    console.log(req.body.user)
+    const createdUser = await createUser(req.body.user);
+    res.send(createdUser);
+})
 
 module.exports = route;
